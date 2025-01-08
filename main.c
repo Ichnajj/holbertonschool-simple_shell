24c70567fcc51baf15a5ad4640b4894ba1c37150
ichnajj <10025@holbertonstudents.com>
2025-01-08 15:47:53 +0100

    shell project

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	char *line = NULL;    /* To store input from the user */
 
-	size_t len = 0;       /* Buffer size for getline */
+	size_t len = 0;
 	ssize_t read;         /* Read status */
 	pid_t pid;            /* Process ID */
 	char *argv[2];  /*a local array if argv isn't part of your fnc signature*/
